6bd782927a57a6ef87447a7b1984939d02f16a51
Albert Rondon <hellomyfrend11@hotmail.com>
2019-02-04 22:45:15 +0000

    did something

--- js/script.js
+++ js/script.js
@@ -4,28 +4,33 @@
 var songs = ["the one","no time","all my friends","swervin","re up","a lonley night"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-var artist = ["Jorja Smith","PnB Rock","21 Savage","A Boggie wit da hoodie","Belly","the weeknd" ];
+var artists = ["Jorja Smith","PnB Rock","21 Savage","A Boggie wit da hoodie","Belly","the weeknd" ];
 var links = ["https://www.youtube.com/watch?v=80J2YroXH3g","https://www.youtube.com/watch?v=1idtRZyTruw","https://www.youtube.com/watch?v=Uqvqbi7iZeA","https://www.youtube.com/watch?v=v-rxFHRTrJc","https://www.youtube.com/results?search_query=re+up+nav","https://www.youtube.com/watch?v=w0nZWYrds9o"];
 var imgLinks = ["https://images.genius.com/467c61d48cff59c00694b1b3a2e1559a.850x850x1.jpg","https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2F2ae153a81de0e8de784287ae35882df3.500x500x1.jpg","https://app.hiphopdx.com/app/uploads/2018/12/181220-21-savage-i-am-i-was-album-cover.jpg","https://i.ytimg.com/vi/3CdiUfQ5_AE/hqdefault.jpg","https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2Ffaca2754ba991ad8ec56320f13b5ae99.1000x1000x1.jpg","https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/The_Weeknd_-_Starboy_%28single%29.jpg/220px-The_Weeknd_-_Starboy_%28single%29.jpg"]; 
 var songLength = [245,248,331,308,419,340];
 
 function displaySongInfo(){
-imgLinks.forEach(function(image) {
+    imgLinks.forEach(function(image) {
         $("#images").append("<img src='" + image + "'>");   
+
     });
-}
-songs.forEach(function(song) {
+    
+     $(".numSongs").html("number of songs " + artists.length);
+    
+    songs.forEach(function(song) {
         $("#songs").append("<p>" + song + "</p>"); 
     });
-artist.forEach(function(artist) {
-     $("#artists").append("<p>" + artist + "</p>")
-});
-links.forEach(function(links) {
-     $("#links").append("<p>" + links + "</p>")
-});
-
+    
+    artists.forEach(function(artist) {
+        $("#artists").append("<p>" + artist + "</p>")
+    });
+    
+    links.forEach(function(links) {
+         $("#links").append("<a href=" + links + ">" + "click here to go to the song" + "</a>")
+    });
+}
 function emptySongInfo(){
- $("#images").empty();
+    $("#images").empty();
     $("#songs").empty();
     $("#artists").empty();
     $("#links").empty();
@@ -41,7 +46,7 @@ function addSongInfo(){
     
     imgLinks.push($("#image").val());
     songs.push($("#song").val());
-    artist.push($("#artist").val());
+    artists.push($("#artist").val());
     links.push($("#link").val());
     songLength.push($("#length").val());
 
@@ -51,9 +56,13 @@ function addSongInfo(){
 
 
 $("#add").click(function() {
+    
     emptySongInfo();
     addSongInfo();
     displaySongInfo();
 });
 
 displaySongInfo();
+  
+  
+
